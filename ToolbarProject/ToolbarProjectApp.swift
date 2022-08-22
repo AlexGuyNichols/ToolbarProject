db3ee73a435d38b58ddd7549249d94239bef8b5e
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-22 15:28:14 +0100

    Initial Commit

---
+++ ToolbarProject/ToolbarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarProjectApp.swift
+//  ToolbarProject
+//
+//  Created by Alex Nichols on 22/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
