15b76cd382084ff2623e2815070c137a89f0f5f8
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-22 16:00:31 +0100

    Played around with toolbars and sheets - then added a toolbar on the sheet

--- ToolbarProject/ContentView.swift
+++ ToolbarProject/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State var isPresenting: Bool = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		NavigationView {
+			Text("Initial View")
+				.toolbar{
+					ToolbarItem(placement: .navigationBarLeading) {
+						Button(action: {
+							
+						}, label: {
+							Image(systemName: "gear")
+						})
+					}
+				}
+				.sheet(isPresented: $isPresenting) {
+					NavigationView {
+						Text("Settings Page")
+							.toolbar {
+								Button("Save") {}
+							}
+					}
+				}
+		}
     }
 }
 
